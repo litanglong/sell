9c6b44505770c316f15f7ecaf30673f9b2d1a3b5
ltl <605325132@qq.com>
2019-08-14 23:20:50 +0800

    加入后台管理模板

--- src/main/java/cn/ltl/sell/controller/SellerOrderController.java
+++ src/main/java/cn/ltl/sell/controller/SellerOrderController.java
@@ -1,16 +1,20 @@
 package cn.ltl.sell.controller;
 
 import cn.ltl.sell.DTO.OrderDTO;
+import cn.ltl.sell.exception.SellException;
 import cn.ltl.sell.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("/seller/order")
 public class SellerOrderController {
@@ -25,5 +29,43 @@ public class SellerOrderController {
         model.addAttribute("orderDTOPage", orderDTOPage);
         model.addAttribute("currentPage", page);
         return "order/list";
+    }
+
+    @GetMapping("{orderId}")
+    public String detail(Model model, @PathVariable String orderId) {
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        model.addAttribute("orderDTO", orderDTO);
+        return "order/detail";
+    }
+
+    @GetMapping("cancel")
+    public String cancel(Model model, String orderId) {
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+        } catch (SellException e) {
+            log.error("【卖家端】取消订单异常，{}", e.getMessage());
+            model.addAttribute("msg", e.getMessage());
+            model.addAttribute("url", "sell/seller/order/list");
+        }
+        return "redirect:list";
+    }
+
+    @GetMapping("finish")
+    public String finish(Model model, String orderId) {
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.finish(orderDTO);
+        } catch (SellException e) {
+            log.error("【卖家端】完结订单异常，{}", e.getMessage());
+            model.addAttribute("msg", e.getMessage());
+            model.addAttribute("url", "sell/seller/order/list");
+        }
+        return "redirect:list";
+    }
+
+    @GetMapping("index")
+    public String index() {
+        return "index";
     }
 }
